9db5af9fe10c235bd989b55a3cd4d9065654039f
cavinginrainbows <13323921+cavinginrainbows@users.noreply.github.com>
2021-01-27 14:11:40 -0800

    added product page

--- techreview/techapp/urls.py
+++ techreview/techapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('gettypes/', views.gettypes, name='types'),
+    path('products/', views.products, name='products'),
 ]
--- techreview/techapp/views.py
+++ techreview/techapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import TechType, Product, Review
 
 # Create your views here.
 def index(request):
     return render(request, 'techapp/index.html')
+
+def gettypes(request):
+    type_list = TechType.objects.all()
+    return render(request, 'techapp/types.html', {'type_list': type_list})
+
+def products(request):
+    product_list = Product.objects.all()
+    return render(request, 'techapp/products.html', {'product_list': product_list})
